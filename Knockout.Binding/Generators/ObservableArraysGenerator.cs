581cbaa55bd0e4c8539518a204da95eea5f6635e
Kevin Boyle <kevin.boyle@red-gate.com>
2013-03-23 14:45:09 +0000

    promote to field

--- Knockout.Binding/Generators/ObservableArraysGenerator.cs
+++ Knockout.Binding/Generators/ObservableArraysGenerator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Knockout.Binding.ExtensionMethods;
 
@@ -7,18 +6,7 @@ namespace Knockout.Binding.Generators
 {
     internal static class ObservableArraysGenerator
     {
-        internal static string GetCollectionsAsObservables(IBindableToJs viewModel)
-        {
-            var observableCollections = viewModel.GetType().ObservableCollectionProperties();
-
-            var observableArrays = observableCollections.Select(info => CreateObservableArrayWithListener(info.Name, viewModel.Name, info.GetValue(viewModel, null))).ToList();
-
-            return String.Join(Environment.NewLine, observableArrays);
-        }
-
-        private static string CreateObservableArrayWithListener(string name, string instanceName, object values)
-        {
-            const string c_ObservableTemplate = @"
+        private const string c_ObservableTemplate = @"
                 this.{0} = ko.observableArray({2});
 
                 this.{0}.subscribe(function(newValue) {{    
@@ -32,6 +20,17 @@ namespace Knockout.Binding.Generators
                 }});
             ";
 
+        internal static string GetCollectionsAsObservables(IBindableToJs viewModel)
+        {
+            var observableCollections = viewModel.GetType().ObservableCollectionProperties();
+
+            var observableArrays = observableCollections.Select(info => CreateObservableArrayWithListener(info.Name, viewModel.Name, info.GetValue(viewModel, null))).ToList();
+
+            return String.Join(Environment.NewLine, observableArrays);
+        }
+
+        private static string CreateObservableArrayWithListener(string name, string instanceName, object values)
+        {
             return String.Format(c_ObservableTemplate, name, instanceName, values.ToJson());
         }
     }
